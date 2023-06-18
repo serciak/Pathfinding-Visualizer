dca02b15325eda67586d45a96ce03c9c31ff6c6e
Serek <jakubseredynski@gmail.com>
2023-06-18 17:12:42 +0200

    maze generator refactoring

--- maze_generator.py
+++ maze_generator.py
@@ -5,88 +5,88 @@ import numpy as np
 
 class MazeGenerator:
     def __init__(self, board_shape):
-        self.h, self.w = board_shape
+        self.rows, self.cols = board_shape
 
     def generate(self):
         self.history = []
 
-        self.board = np.ones((self.h, self.w))
+        self.board = np.ones((self.rows, self.cols))
         self.board[1:-1,1:-1] = 0
 
-        print(self.w, self.h)
-        top_border = [(i, 0) for i in range(self.h)]
-        right_border = [(self.h-1, i) for i in range(self.w)]
-        bottom_border = [(i, self.w-1) for i in range(self.h)]
-        left_border = [(0, i) for i in range(self.w)]
+        print(self.cols, self.rows)
+        top_border = [(i, 0) for i in range(self.rows)]
+        right_border = [(self.rows-1, i) for i in range(self.cols)]
+        bottom_border = [(i, self.cols-1) for i in range(self.rows)]
+        left_border = [(0, i) for i in range(self.cols)]
         
         self.history.extend(top_border)
         self.history.extend(right_border)
         self.history.extend(reversed(bottom_border))
         self.history.extend(reversed(left_border))
         
-        self.divide(0, 0, self.w, self.h)
+        self.divide(0, 0, self.cols, self.rows)
 
         return self.board, self.history
 
-    def divide(self, x, y, w, h):
+    def divide(self, x, y, cols, rows):
         random_generator = random.Random()
 
         orientation = 0
         new_wall = 0
         new_hole = 0
-        new_h = 0
-        new_w = 0
+        new_rows = 0
+        new_cols = 0
         y_pair = 0
         x_pair = 0
-        new_h_pair = 0
-        new_w_pair = 0
+        new_rows_pair = 0
+        new_cols_pair = 0
 
-        if w < h:
+        if cols < rows:
             orientation = 0
-        elif w > h:
+        elif cols > rows:
             orientation = 1
         else:
             orientation = random_generator.randint(0, 1)
 
         if orientation == 0:
-            if h < 5:
+            if rows < 5:
                 return
 
-            new_wall = y + (random_generator.randint(2, h - 3) // 2) * 2
-            new_hole = x + (random_generator.randint(1, w - 2) // 2) * 2 + 1
+            new_wall = y + (random_generator.randint(2, rows - 3) // 2) * 2
+            new_hole = x + (random_generator.randint(1, cols - 2) // 2) * 2 + 1
 
-            for i in range(x, x + w - 1):
+            for i in range(x, x + cols - 1):
                 self.board[new_wall, i] = 1
                 self.history.append((new_wall, i))
             self.board[new_wall, new_hole] = 0
             self.history.append((new_wall, new_hole))
 
-            new_h = new_wall - y + 1
-            new_w = w
+            new_rows = new_wall - y + 1
+            new_cols = cols
 
             y_pair = new_wall
             x_pair = x
-            new_h_pair = y + h - new_wall
-            new_w_pair = w
+            new_rows_pair = y + rows - new_wall
+            new_cols_pair = cols
         elif orientation == 1:
-            if w < 5:
+            if cols < 5:
                 return
 
-            new_wall = x + (random_generator.randint(2, w - 3) // 2) * 2
-            new_hole = y + (random_generator.randint(1, h - 2) // 2) * 2 + 1
-            for i in range(y, y + h - 1):
+            new_wall = x + (random_generator.randint(2, cols - 3) // 2) * 2
+            new_hole = y + (random_generator.randint(1, rows - 2) // 2) * 2 + 1
+            for i in range(y, y + rows - 1):
                 self.board[i, new_wall] = 1
                 self.history.append((i, new_wall))
             self.board[new_hole][new_wall] = 0
             self.history.append((new_hole, new_wall))
 
-            new_h = h
-            new_w = new_wall - x + 1
+            new_rows = rows
+            new_cols = new_wall - x + 1
 
             y_pair = y
             x_pair = new_wall
-            new_h_pair = h
-            new_w_pair = x + w - new_wall
+            new_rows_pair = rows
+            new_cols_pair = x + cols - new_wall
 
-        self.divide(x, y, new_w, new_h)
-        self.divide(x_pair, y_pair, new_w_pair, new_h_pair)
+        self.divide(x, y, new_cols, new_rows)
+        self.divide(x_pair, y_pair, new_cols_pair, new_rows_pair)
